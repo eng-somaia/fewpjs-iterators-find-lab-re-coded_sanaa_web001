4e47053b24bc1c1669dd4dfa51f9e39e610581ac
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-25 10:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function testFunc() {
   return "hi"
 }
 
-function superbowlWin(x){let z = x.find(a => a.result === "W")
- if( )
+function superbowlWin(x){
+  let z = x.find(a => a.result === "W")
+ if(z)
     return z.year;
     else return undefined;
 }
